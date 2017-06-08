381b1a36220ed5126b01b3b05c5f127c2b6eb521
bhaumeen <bhaumeen@users.noreply.github.com>
2017-06-09 03:42:41 +0530

    Add files via upload
    added ui for upload docs ....added button that opens file explorer and allows user to select files to be uploaded !

---
+++ BKSite/BKSite/src/com/bk/site/DbHelper.java
@@ -0,0 +1,72 @@
+package com.bk.site;
+
+import java.sql.*;
+
+
+
+public class DbHelper
+{
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/nvc";
+	Connection con=null;
+	PreparedStatement pstmt=null;
+	public DbHelper()
+	{
+		
+	}
+	
+	public boolean insertUser(UserClass uc)
+	{
+		String query="insert into user_table (user_name,user_fname,user_email,user_password) values (?,?,?,?)";
+		try
+		{
+			Class.forName(JDBC_DRIVER);
+			con=DriverManager.getConnection(DB_URL, "root", "bhaumeen98");
+			pstmt=con.prepareStatement(query);
+			pstmt.setString(1, uc.uname);
+			pstmt.setString(2, uc.fname);
+			pstmt.setString(3, uc.email);
+			pstmt.setString(4, uc.pass);
+			pstmt.execute();
+			con.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return true;
+	}
+	
+	public String fetchUser(UserClass uc)
+	{
+		String query="select * from user_table where user_name='"+uc.uname+"'";
+		String uname="",email="",passwd="",fname="";
+		try
+		{
+			Class.forName(JDBC_DRIVER);
+			con=DriverManager.getConnection(DB_URL, "root", "bhaumeen98");
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery(query);
+			if(rs!=null)
+			while(rs.next())
+			{
+				uname=rs.getString("user_name");
+				fname=rs.getString("user_fname");
+				email=rs.getString("user_email");
+				passwd=rs.getString("user_password");
+			}
+			if(uc.uname.equals(uname) && uc.pass.equals(passwd))
+			{
+				System.out.println("It's a match!\nFull Name: "+fname);
+				return fname;
+			}
+			else
+				return null;
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
---
+++ BKSite/BKSite/src/com/bk/site/TestServlet.java
@@ -0,0 +1,105 @@
+package com.bk.site;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+@SuppressWarnings("serial")
+public class TestServlet extends HttpServlet
+{
+	UserClass uc;
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException
+	{
+		
+	}
+	public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException
+	{
+		System.out.println("Servlet called");
+		BufferedReader br=new BufferedReader(new InputStreamReader(request.getInputStream()));
+		String objParam="";
+		if(br!=null)
+		{
+			objParam=br.readLine();
+		}
+		System.out.println("Json Received: "+objParam);
+		try
+		{
+			Gson gson=new Gson();
+			uc=gson.fromJson(objParam, UserClass.class);
+			System.out.println("Printing parsed Json below");
+			uc.printUser();
+			if(uc.action.equals("register"))
+			{
+				boolean createStatus=createUser();
+				response.setContentType("application/json");
+				PrintWriter out=response.getWriter();
+				String insertResponse="";
+				if(createStatus)
+				{
+					System.out.println("User created successfully.");
+					insertResponse="{\"createResult\":\"success\"}";
+					out.print(insertResponse);
+					out.flush();
+				}
+				else
+				{
+					System.out.println("Some error occured in user creation.");
+					insertResponse="{\"createResult\":\"failure\"}";
+					out.print(insertResponse);
+					out.flush();
+				}
+			}
+			else if(uc.action.equals("login"))
+			{
+				response.setContentType("application/json");
+				PrintWriter out=response.getWriter();
+				String loginRes=loginUser();
+				String jsonResponse="";
+				if(loginRes!=null)
+				{
+					System.out.println("Login successful.");
+					jsonResponse="{\"result\":\"success\",\"fname\":\""+loginRes+"\"}";
+					out.print(jsonResponse);
+					out.flush();
+				}
+				else
+				{
+					System.out.println("Login failed.");
+					jsonResponse="{\"result\":\"failure\",\"fname\":\"\"}";
+					out.print(jsonResponse);
+					out.flush();
+				}
+			}
+		}
+		catch(Exception err)
+		{
+			err.printStackTrace();
+		}
+	}
+	private boolean createUser()
+	{
+		if(uc!=null)
+		{
+			DbHelper dh=new DbHelper();
+			boolean insertStatus=dh.insertUser(uc);
+			uc=null;
+			return insertStatus;
+		}
+		else
+			return false;
+	}
+	private String loginUser()
+	{
+		DbHelper dh=new DbHelper();
+		String result=dh.fetchUser(uc);
+		return result;
+	}
+}
